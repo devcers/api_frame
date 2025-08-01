7cd4aa40666b537d28fb7fa8bc572fec3fa8ff29
lili <devcer@163.com>
2025-08-01 18:43:22 +0800

    init project

---
+++ run.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : run.py
+@Time    : 2025/8/1 18:26
+@Author  : 昭阳
+@Email   : devcer@163.com
+@Description : 
+"""
+
+if __name__ == '__main__':
+    pytest.main()
---
+++ testcases/test_api.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+@File    : test_api.py
+@Time    : 2025/8/1 18:15
+@Author  : 昭阳
+@Email   : devcer@163.com
+@Description : 
+"""
+
+class TestApi:
+
+    def test_api(self):
+        print("test_api")
